6b4f5e167b7af233cd901141dccaacf031ebb1f6
Andrii Olkhovych <the.def4alt@gmail.com>
2024-01-02 12:55:50 +0100

    Cleanup default template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,7 @@
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<footer>©2024 Andrii Olkhovych</footer>
 </div>
 
 <style>
@@ -38,16 +36,6 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
 		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
